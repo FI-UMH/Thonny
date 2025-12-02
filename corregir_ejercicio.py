36f16011d583dc1005339648989f17ee57cb82c8
Introducción a Python <fi@umh.es>
2025-12-02 12:47:07 +0100

    Update corregir_ejercicio.py

--- corregir_ejercicio.py
+++ corregir_ejercicio.py
@@ -855,7 +855,7 @@ def _corregir_ejercicio_funcion(dni, ejercicio, fuente, lista_tests):
 
 def main():
     global _TESTS_CACHE
-    _TEST_CACHE = None
+    _TESTS_CACHE = None
     wb = get_workbench()
     ed = wb.get_editor_notebook().get_current_editor()
 
